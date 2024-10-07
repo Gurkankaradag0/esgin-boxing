029e51b266a43c085a6c67911ae739fd51869e59
Gürkan Karadağ <gurkan.karadag.1907@gmail.com>
2024-10-07 23:32:48 +0300

    remove formik and yup

--- app/(site)/(client)/auth/register/page.js
+++ app/(site)/(client)/auth/register/page.js
@@ -9,24 +9,6 @@ export const metadata = {
 
 const RegisterPage = () => {
     return (
-        // <div className='flex flex-col w-min m-auto gap-8 justify-center items-center'>
-        //     <h2 className='text-3xl font-semibold'>Kayıt Ol</h2>
-        //     <div className='flex flex-col gap-2 justify-center items-center border border-solid p-8 rounded-xl'>
-        //         <RegisterForm />
-        //         <hr className='w-full' />
-        //         <div className='flex w-full justify-center items-center text-xs whitespace-nowrap'>
-        //             <span>Zaten bir hesabın var mı?</span>
-        //             <Link href='/auth/login'>
-        //                 <Button
-        //                     variant='link'
-        //                     size='sm'
-        //                 >
-        //                     Giriş Yap
-        //                 </Button>
-        //             </Link>
-        //         </div>
-        //     </div>
-        // </div>
         <Card>
             <CardHeader>
                 <CardTitle className='text-xl'>Hesap oluştur</CardTitle>
