79587a20214f121d6cac43b57d47012c69878c17
Gürkan Karadağ <gurkan.karadag.1907@gmail.com>
2024-10-08 00:51:20 +0300

    restore footer border color

--- app/components/Layout/Footer.js
+++ app/components/Layout/Footer.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Footer = () => {
     return (
-        <footer className='border-t border-solid border-black/10 dark:border-white/10 py-4 px-8'>
+        <footer className='border-t border-solid border-muted py-4 px-8'>
             <Link
                 href='#'
                 className='hover:underline text-xs flex justify-end items-center'
